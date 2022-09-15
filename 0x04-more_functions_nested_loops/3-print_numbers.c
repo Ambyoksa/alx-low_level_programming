9e2b34c36ebbd08902421d60c707885bf4a63a59
Ambyoksa <andrewyoksa@yahoo.co.uk>
2022-09-15 08:48:16 -0700

    3. The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _putchar - unction that prints the numbers, from 0 to 9
+ * @n: umbers to be printed
+ * Return: numbers 0 to 9
+ */
+
+int _putchar(int n)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+		_putchar(0, n++);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
