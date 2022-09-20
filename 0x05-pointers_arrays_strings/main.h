9005365be1331a44a8655c63a16d70dc0dbda777
Ambyoksa <andrewyoksa@yahoo.co.uk>
2022-09-20 10:48:23 -0700

    Task 0

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include<stdio.h>
+#include <stdio.h>
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -16,4 +16,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
-endif /*MAIN-H*/
+endif
