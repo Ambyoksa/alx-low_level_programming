baa3056391783c31d2a38ecfa858cdd831e5d2b0
Ambyoksa <andrewyoksa@yahoo.co.uk>
2022-09-26 06:45:06 -0700

    copies memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _memcpy - function that copy memory
+ * @dest: destination memory
+ * @src: source memory
+ * @n: interger
+ * Return: pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n);
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+		dset[a] = src[a];
+	return (dest);
+}
