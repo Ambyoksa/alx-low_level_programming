46103e3167a9a8e7df95d34e0dbd1a71a80f5c9a
Ambyoksa <andrewyoksa@yahoo.co.uk>
2022-09-26 07:06:48 -0700

    function that copies memory area

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,11 +8,11 @@
  * Return: pointer to dest
  */
 
-char *_memcpy(char *dest, char *src, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int a;
 
 	for (a = 0; a < n; a++)
-		dset[a] = src[a];
+		dest[a] = src[a];
 	return (dest);
 }
