69c67cf478fe80ab2d1c2a25b0d29213a9b24f96
Ambyoksa <andrewyoksa@yahoo.co.uk>
2022-10-04 02:07:43 -0700

    multiplies two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,15 +11,14 @@
 
 int main(int argc, char *argv[])
 {
-	int a, b;
+	/* validate input */
+	if (argc != 3)
+		{
+		printf("Error\n");
+			return (1);
+		}
 
-	if (argc == 3)
-	{
-		a = atoi(argv[1]);
-		b = atoi(argc[2]);
-		printf("%d\n", a *b);
+		/* mulitply two arguments passed via cmd line */
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+
 		return (0);
-	}
-	printf("Error\n");
-	return (1);
-}
