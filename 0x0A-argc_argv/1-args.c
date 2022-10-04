4f3166bfd1ab7a22033ef2a2e93a7dcf142eaa73
Ambyoksa <andrewyoksa@yahoo.co.uk>
2022-10-04 00:36:39 -0700

    prints all arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,17 +2,17 @@
 #include "main.h"
 
 /**
- * main - print name of program
- * @argc: count arguments
- * @argv: arguments
- *
- * Return: Always 0
- */
+* main - print name of program
+* @argc: count arguments
+* @argv: arguments
+*
+* Return: Always 0
+*/
 
 int main(int argc, char *argv[])
 {
-	(void) argv; /*ignore argv*/
-	printf("%i\n", argc - 1);
+(void) argv; /*ignore argv*/
+printf("%i\n", argc - 1);
 
-	return (0);
+return (0);
 }
